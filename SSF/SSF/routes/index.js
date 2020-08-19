d92c634b6e2d54bbc8831f924742570b3b9e0a07
leon123858 <a0970785699@gmail.com>
2020-08-20 00:48:33 +0800

    add FW html

--- SSF/SSF/routes/index.js
+++ SSF/SSF/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' ,num:3});
+    res.render('Page1', { title: '<h1>Express</h1>' ,num:3});
 });
 
 module.exports = router;
