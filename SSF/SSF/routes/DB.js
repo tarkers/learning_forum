82fc56d88218afa5f4c2f9f00bb577afd290d198
leon123858 <a0970785699@gmail.com>
2020-08-24 22:49:15 +0800

    page9 prototype successful

--- SSF/SSF/routes/DB.js
+++ SSF/SSF/routes/DB.js
@@ -1,6 +1,7 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var urlencode = require('urlencode');
 /* GET users listing. */
 
 //router.post('/to_post_page', function (req, res) {
@@ -12,6 +13,11 @@ router.post('/post_public', function (req, res) {
 router.post('/post_private', function (req, res) {
     //res.render('Page9', { ID: req.body.ID, board_ID: req.body.board_ID ,type: req.body.type});
     console.log(req.body);
+});
+
+router.get('/post_preview', function (req, res) {
+    //render('preview', { html: req.params.html });
+    res.render('preview', { html: urlencode.decode(req.query.html) });
 });
 
 module.exports = router;
