7cfcb5d5006bec228b16df62ce0148d5318b311c
leon123858 <a0970785699@gmail.com>
2020-08-26 01:59:54 +0800

    ./post_public實現

--- SSF/SSF/routes/DB.js
+++ SSF/SSF/routes/DB.js
@@ -2,12 +2,37 @@
 var express = require('express');
 var router = express.Router();
 var urlencode = require('urlencode');
-/* GET users listing. */
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
 
+//jump to public board
+function jumpPublic(board_ID,ID,res) {
+    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection(board_ID);
+        var findThing = {};
+        table.find(findThing, { projection: { _id: 0} }).toArray(function (err, result) {
+            if (err) throw err;
+            db.close();
+            console.log(result);
+            res.render('Page8', { ID : ID, data: result, num: result.length });
+        });
+    });
+}
 //router.post('/to_post_page', function (req, res) {
 router.post('/post_public', function (req, res) {
-    //res.render('Page9', { ID: req.body.ID, board_ID: req.body.board_ID ,type: req.body.type});
     console.log(req.body);
+    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection(req.body.board_ID);
+        var insertThing = { ID: req.body.ID, title: req.body.title, include: req.body.include };
+        table.insertOne( insertThing , function (err, result) {
+            if (err) throw err;
+            db.close();
+            console.log(result);
+            jumpPublic(req.body.board_ID, req.body.ID,res);
+        });
+    });
 });
 
 router.post('/post_private', function (req, res) {
--- SSF/SSF/routes/JB.js
+++ SSF/SSF/routes/JB.js
@@ -5,7 +5,7 @@ var router = express.Router();
 //router.post('/to_post_page', function (req, res) {
 router.get('/to_post_page', function (req, res) {
     //res.render('Page9', { ID: req.body.ID, board_ID: req.body.board_ID ,type: req.body.type});
-    res.render('Page9', { ID: 'leon1234858', board_ID: '0001', type: 'private' });
+    res.render('Page9', { ID: 'leon1234858', board_ID: 'public_0001', type: 'public' });
 });
 
 module.exports = router;
