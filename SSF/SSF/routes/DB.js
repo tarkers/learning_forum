018107328af49e624fdc99052bf59255e3619f7e
leon123858 <a0970785699@gmail.com>
2020-08-26 10:50:08 +0800

    post public api success (debug fo no num parameter in it)

--- SSF/SSF/routes/DB.js
+++ SSF/SSF/routes/DB.js
@@ -4,9 +4,34 @@ var router = express.Router();
 var urlencode = require('urlencode');
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
-
+//updataNum
+function getCount_public(req,res) {
+    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection(req.body.board_ID);
+        var findThing = {};
+        table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (err) throw err;
+            db.close();
+            insert_public(req, res, result.length);
+        });
+    });
+}
+//insert public
+function insert_public(req, res,num) {
+    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection(req.body.board_ID);
+        var insertThing = { num: num+1 ,ID: req.body.ID, title: req.body.title, include: req.body.include };
+        table.insertOne(insertThing, function (err, result) {
+            if (err) throw err;
+            db.close();
+            jumpPublic(req.body.board_ID, req.body.ID, res);
+        });
+    });
+}
 //jump to public board
-function jumpPublic(board_ID,ID,res) {
+function jumpPublic(board_ID, ID, res) {
     MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var table = db.db("board").collection(board_ID);
@@ -14,7 +39,6 @@ function jumpPublic(board_ID,ID,res) {
         table.find(findThing, { projection: { _id: 0} }).toArray(function (err, result) {
             if (err) throw err;
             db.close();
-            console.log(result);
             res.render('Page8', { ID : ID, data: result, num: result.length });
         });
     });
@@ -22,17 +46,7 @@ function jumpPublic(board_ID,ID,res) {
 //router.post('/to_post_page', function (req, res) {
 router.post('/post_public', function (req, res) {
     console.log(req.body);
-    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
-        if (err) throw err;
-        var table = db.db("board").collection(req.body.board_ID);
-        var insertThing = { ID: req.body.ID, title: req.body.title, include: req.body.include };
-        table.insertOne( insertThing , function (err, result) {
-            if (err) throw err;
-            db.close();
-            console.log(result);
-            jumpPublic(req.body.board_ID, req.body.ID,res);
-        });
-    });
+    getCount_public(req, res);
 });
 
 router.post('/post_private', function (req, res) {
@@ -43,6 +57,21 @@ router.post('/post_private', function (req, res) {
 router.get('/post_preview', function (req, res) {
     //render('preview', { html: req.params.html });
     res.render('preview', { html: urlencode.decode(req.query.html) });
+});
+
+router.get('/get_board_introduce', function (req, res) {
+    //render('preview', { html: req.params.html });
+    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection('all_board_number');
+        var findThing = { ID: req.query.board_ID };
+        table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (err) throw err;
+            db.close();
+            console.log(result);
+            res.send({ data: result, num: result.length });
+        });
+    });
 });
 
 module.exports = router;
