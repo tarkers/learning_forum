0587f19cfcd7f256c5b84db9e75d5ab1a1a0c972
leon123858 <a0970785699@gmail.com>
2020-08-27 04:17:31 +0800

    page8 prototype OK

--- SSF/SSF/routes/DB.js
+++ SSF/SSF/routes/DB.js
@@ -4,42 +4,62 @@ var router = express.Router();
 var urlencode = require('urlencode');
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
+var uri = "mongodb://localhost:27017/";
+//getpassword
+function getPassword_private(req,res) {
+    MongoClient.connect(uri , { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection("all_board_number");
+        var findThing = { board_ID: req.body.board_ID };
+        table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (err) throw err;
+            db.close();
+            console.log(result[0]['password'] == req.body.board_password)
+            if (result[0]['password'] == req.body.board_password) {
+                getCount_public(req, res);
+            }
+        });
+    });
+}
 //updataNum
 function getCount_public(req,res) {
-    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var table = db.db("board").collection(req.body.board_ID);
         var findThing = {};
         table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
             if (err) throw err;
             db.close();
+            //console.log(result);
             insert_public(req, res, result.length);
         });
     });
 }
 //insert public
 function insert_public(req, res,num) {
-    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var table = db.db("board").collection(req.body.board_ID);
         var insertThing = { num: num+1 ,ID: req.body.ID, title: req.body.title, include: req.body.include };
         table.insertOne(insertThing, function (err, result) {
             if (err) throw err;
             db.close();
+            //console.log(result);
             jumpPublic(req.body.board_ID, req.body.ID, res);
         });
     });
 }
 //jump to public board
 function jumpPublic(board_ID, ID, res) {
-    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var table = db.db("board").collection(board_ID);
         var findThing = {};
         table.find(findThing, { projection: { _id: 0} }).toArray(function (err, result) {
             if (err) throw err;
             db.close();
-            res.render('Page8', { ID : ID, data: result, num: result.length });
+            //console.log(result);
+            res.render('Page8', { board_ID: board_ID , ID: ID, data: result, num: result.length });
         });
     });
 }
@@ -50,21 +70,19 @@ router.post('/post_public', function (req, res) {
 });
 
 router.post('/post_private', function (req, res) {
-    //res.render('Page9', { ID: req.body.ID, board_ID: req.body.board_ID ,type: req.body.type});
     console.log(req.body);
+    getPassword_private(req, res);
 });
 
 router.get('/post_preview', function (req, res) {
-    //render('preview', { html: req.params.html });
     res.render('preview', { html: urlencode.decode(req.query.html) });
 });
-
+//test data  { "_id" : ObjectId("5f467c9f452c6bd802ab5974"), "board_ID" : "private_0002", "type" : "private", "title" : "test", "introduce" : "<h1>testtesttest</h1>", "password" : "0000" }
 router.get('/get_board_introduce', function (req, res) {
-    //render('preview', { html: req.params.html });
-    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, function (err, db) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var table = db.db("board").collection('all_board_number');
-        var findThing = { ID: req.query.board_ID };
+        var findThing = { board_ID: req.query.board_ID };
         table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
             if (err) throw err;
             db.close();
@@ -72,6 +90,10 @@ router.get('/get_board_introduce', function (req, res) {
             res.send({ data: result, num: result.length });
         });
     });
+});
+
+router.post('/discuss', function (req, res) {
+    console.log(req.body);
 });
 
 module.exports = router;
