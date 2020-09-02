4a7048de1a84919ccdcf2fc4b9ce7e3699e028c1
leon123858 <a0970785699@gmail.com>
2020-09-02 21:05:40 +0800

    DB and JB OK 尚未進行測試

--- SSF/SSF/routes/DB.js
+++ SSF/SSF/routes/DB.js
@@ -8,23 +8,26 @@ var uri = "mongodb://localhost:27017/";
 
 //function block
 
-//getpassword
+//post public && post private
+
 function getPassword_private(req,res) {
     MongoClient.connect(uri , { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
-        var table = db.db("board").collection("all_board_number");
+        var table = db.db("people").collection("manager_information");
         var findThing = { board_ID: req.body.board_ID };
         table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
             if (err) throw err;
             db.close();
-            console.log(result[0]['password'] == req.body.board_password)
-            if (result[0]['password'] == req.body.board_password) {
-                getCount_public(req, res);
+            if (result.length > 0) {
+                console.log(result[0]['public_password'] == req.body.password)
+                if (result[0]['public_password'] == req.body.password) {
+                    getCount_public(req, res);
+                }
             }
         });
     });
 }
-//updataNum
+
 function getCount_public(req,res) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
@@ -38,7 +41,7 @@ function getCount_public(req,res) {
         });
     });
 }
-//plus follower 建立追蹤者區
+
 function plus_follower(req, num) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
@@ -51,7 +54,7 @@ function plus_follower(req, num) {
         });
     });
 }
-//insert public + plus  follower block
+
 function insert_public(req, res,num) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
@@ -66,7 +69,7 @@ function insert_public(req, res,num) {
         });
     });
 }
-//jump to public board
+
 function jumpPublic(board_ID, ID, res) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
@@ -81,76 +84,131 @@ function jumpPublic(board_ID, ID, res) {
     });
 }
 
+//add lover
+function add_lover(req,res) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("people").collection("personal_lover");
+        var filter = { ID: req.query.ID };
+        var goal = {};
+        goal[req.query.board_ID] = req.query.board_ID;
+        table.updateOne(filter, { $set: goal }, function (err, result) {
+            if (err) {
+                res.end({ result: 'error' });
+                throw err;
+            }
+            db.close();
+            //console.log(result);
+            res.end({ result: 'success' });
+        });
+    });
+}
 
+//discuss
+function discuss_getNum(req, res) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection(req.body.board_ID);
+        var findThing = { num: req.body.num };
+        table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (err) throw err;
+            db.close();
+            //console.log(result);
+            discuss_update(req, res, Object.keys(result[0]).length - 5 + 1);
+        });
+    });
+}
+
+function discuss_update(req, res, num) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection(req.body.board_ID);
+        var filter = { num: req.body.num };
+        var goal = {};
+        goal[num.toString()] = req.body.ID + ':' + req.body.include;
+        table.updateOne(filter, { $set: goal }, function (err, result) {
+            if (err) {
+                res.end({ result: 'error' });
+                throw err;
+            }
+            db.close();
+            //console.log(result);
+            discuss_notice(req);
+            res.end({ result: 'success' });
+        });
+    });
+}
+
+function discuss_notice(req) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("follow").collection(req.body.board_ID);
+        var findThing = { num: req.body.num };
+        table.find(findThing, { projection: { _id: 0,num:0 } }).toArray(function (err, result) {
+            if (err) throw err;
+            db.close();
+            //console.log(result);
+            discuss_notice_add(req, result[0]);
+        });
+    });
+}
+
+function discuss_notice_add(req,data) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("follow").collection(req.body.board_ID);
+        var filter = { num: req.body.num };
+        var goal = {};
+        goal[req.body.ID] = req.body.ID;
+        table.updateOne(filter, { $set: goal }, function (err, result) {
+            if (err) { throw err;}
+            db.close();
+            //console.log(result);
+            var list = Object.keys(data);
+            for (var i in list) 
+                discuss_notice_send(i, req.body.board_ID + '_' + req.body.num);
+        });
+    });
+}
+
+function discuss_notice_send(id,key) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("people").collection("personal_notice");
+        var filter = { ID: id };
+        var goal = {};
+        goal[key] = key;
+        table.updateOne(filter, { $set: goal }, function (err, result) {
+            if (err) { throw err; }
+            db.close();
+            console.log(id+'->' + key);
+        });
+    });
+}
 
 //router block
-
-//post: ‘./DB/post_public’, parameter -> ID, board_ID, title, include
-//tmp1 = board.p_board_ID.find()
-//num = tmp1.length + 1 //知道是該版第幾則貼文
-//board.p_board_ID.insert({ num: num, ID: p_ID, title: p_title, include: p_include, hide: false })
-//follow.p_board_ID.insert({ num: num })
-//tmp2 = board.p_board_ID.find(hide == false)
-//render('page8', { data: tmp2, num: tmp2.length, board_ID: p_board_ID, ID: p_ID })
 router.post('/post_public', function (req, res) {
-    console.log(req.body);
+    //console.log(req.body);
     getCount_public(req, res);
 });
 
-
-//post: ‘./DB/post_private’, parameter -> ID, board_ID, title, include, password
-//tmp1 = people.manager_information.find(board_ID == p_board_ID)
-//if tmp1['password_public'] == p_password && tmp1.length > 0
-//    tmp2 = board.p_board_ID.find()
-//num = tmp2.length + 1 //知道是該版第幾則貼文
-//board.p_board_ID.insert({ num: num, ID: p_ID, title: p_title, include: p_include, hide: false })
-//follow.p_board_ID.insert({ num: num })
-//tmp3 = board.p_board_ID.find(hide == false)
-//render('page8', { data: tmp3, num: tmp3.length, board_ID: p_board_ID, ID: p_ID })
 router.post('/post_private', function (req, res) {
-    console.log(req.body);
+    //console.log(req.body);
     getPassword_private(req, res);
 });
 
-//get: ‘./DB/post_preview’ parameter -> html
-//render('preview', { html: p_html })
 router.get('/post_preview', function (req, res) {
     res.render('preview', { html: urlencode.decode(req.query.html) });
 });
 
-
-//get: ‘./DB/add_lover’ , parameter -> ID, board_ID
-//filter < - ID == p_ID
-//goal = {}
-//goal[p_board_ID] = p_board_ID
-//people.personal_lover.update(filter, goal)
-//if success -> return { result: success }(用 res.end(JSON))
-//else -> return { result: error }
 router.get('/add_lover', function (req, res) {
-    res.render('preview', { html: urlencode.decode(req.query.html) });
+    //res.render('preview', { html: urlencode.decode(req.query.html) });
+    add_lover(req, res);
 });
 
-
-//get: ‘./DB/discuss’ parameter -> num, ID, board_ID, include
-//tmp1 = board.p_board_ID.find(num == p_num)
-//count = tmp1.length - 5 + 1 //因為有五個非留言參數
-//filter < - num = p_num
-//goal = {}
-//goal[count] = include
-//board.p_board_ID.update(filter, $set(goal))
-////以上完成留言,以下進行通知
-//tmp2 = follow.p_board_ID.find({ 不顯示num }, num == p_num)
-//goal2 = {}
-//str = p_board + '_' + p_num
-//goal2[str] = str
-//for (i in tmp2)
-//    filter < - ID == i
-//people.personal_notice.updateOne(filter, $set(goal2))
-//goal3 = {}
-//goal3[p_ID] = p_ID
-//follow.p_board_ID.updateOne({ num == p_num}, $set{ goal3 })
 router.post('/discuss', function (req, res) {
-    console.log(req.body);
+    //console.log(req.body);
+    discuss_getNum(req, res);
 });
 
 module.exports = router;
--- SSF/SSF/routes/JB.js
+++ SSF/SSF/routes/JB.js
@@ -4,40 +4,47 @@ var router = express.Router();
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
 var uri = "mongodb://localhost:27017/";
-/* GET users listing. */
-function jumpPublic(board_ID, ID, res) {
+
+//to public & private board
+
+function test_type(req, res ,type) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
-        var table = db.db("board").collection(board_ID);
+        var table = db.db("people").collection("manager_information");
+        var findThing = { board_ID: req.body.board_ID };
+        table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (err || result.length == 0) throw err;
+            db.close();
+            //console.log(result);
+            if (result[0]['type'] == type && type == 'public') {
+                jumpboard(req, res);
+            }
+            else if (result[0]['type'] == type && type == 'private' && result[0]['password_public'] == req.body.password) {
+                jumpboard(req, res);
+            }
+        });
+    });
+}
+
+function jumpboard(req, res) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("board").collection(req.body.board_ID);
         var findThing = { hide: 'false' };
         table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
             if (err) throw err;
             db.close();
             //console.log(result);
-            res.render('Page8', { board_ID: board_ID, ID: ID, data: result, num: result.length });
+            var pkg = { board_ID: board_ID, ID: ID, data: result, num: result.length };
+            if (req.body.place)
+                pkg['place'] = req.body.place;
+            res.render('Page8', pkg );
         });
     });
 }
-router.post('/to_post_page', function (req, res) {
-//router.get('/to_post_page', function (req, res) {
-    //res.render('Page9', { ID: req.body.ID, board_ID: req.body.board_ID ,type: req.body.type});
-    res.render('Page9', { ID: 'leon1234858', board_ID: 'private_0002', type: 'private' });
-});
 
-//toprivate 要多做密碼檢測 還沒做
-//router.post('/to_post_page', function (req, res) {
-router.get('/to_private_board', function (req, res) {
-    jumpPublic('private_0002', 'leon1234858', res);
-});
-
-//router.post('/to_post_page', function (req, res) {
-router.get('/to_public_board', function (req, res) {
-   // jumpPublic(req.body.board_ID, req.body.ID, res);
-    jumpPublic('public_0002', 'leon1234858', res);
-});
-
-//test data  { "_id" : ObjectId("5f467c9f452c6bd802ab5974"), "board_ID" : "private_0002", "type" : "private", "title" : "test", "introduce" : "<h1>testtesttest</h1>", "password" : "0000" }
-router.get('/get_board_introduce', function (req, res) {
+//get introduce
+function get_board_information(req, res) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var table = db.db("board").collection('all_board_number');
@@ -46,9 +53,28 @@ router.get('/get_board_introduce', function (req, res) {
             if (err) throw err;
             db.close();
             console.log(result);
-            res.send({ data: result, num: result.length });
+            res.send({ title: result[0]['title'], introduce: result[0]['introduce'] });
         });
     });
+}
+
+//to post
+
+router.post('/to_public_board', function (req, res) {
+    test_type(req, res,'public');
+});
+
+router.post('/to_private_board', function (req, res) {
+    test_type(req, res,'private');
+});
+
+router.get('/get_board_introduce', function (req, res) {
+    get_board_information(req, res);
+});
+
+router.post('/to_post_page', function (req, res) {
+    res.render('Page9', { ID: req.body.ID, board_ID: req.body.board_ID ,type: req.body.type});
+    //res.render('Page9', { ID: 'leon1234858', board_ID: 'private_0002', type: 'private' });
 });
 
 module.exports = router;
