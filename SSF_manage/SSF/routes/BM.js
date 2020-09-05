85769ff7129af50d2ae40b70834960adf5000bcf
leon123858 <a0970785699@gmail.com>
2020-09-05 13:16:57 +0800

    MB.js 完成未測試 BM.js 架構建好

--- SSF_manage/SSF/routes/BM.js
+++ SSF_manage/SSF/routes/BM.js
@@ -1,9 +1,44 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
-/* GET users listing. */
-router.get('/', function (req, res) {
-    res.send('respond with a resource');
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+var uri = "mongodb://localhost:27017/";
+var core_ID = 'admin';
+var core_password = '0000';
+
+//function
+
+
+
+//router
+//前往管理者建立頁
+router.post('/to_manager_build', function (req, res) {
+    
+});
+//前往管理者資料更新頁
+router.post('/to_manager_update', function (req, res) {
+
+});
+//管理者註冊(建立)
+router.post('/build', function (req, res) {
+
+});
+//(AJAX)更新管理者註冊資料
+router.post('/update', function (req, res) {
+
+});
+//(AJAX)獲得管理者註冊資料
+router.post('/get_information', function (req, res) {
+
+});
+//管理者登入
+router.post('/login', function (req, res) {
+
+});
+//核心管理者登入
+router.post('/core_login', function (req, res) {
+
 });
 
 module.exports = router;
--- SSF_manage/SSF/routes/MB.js
+++ SSF_manage/SSF/routes/MB.js
@@ -20,6 +20,21 @@ function IsPasswordRight(req, res, next, other) {
     });
 }
 
+function ReJSON(req, res) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("people").collection("manager_information");
+        var findThing = { board_ID: req.body.board_ID };
+        table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (err) {
+                res.json({ result: 'error' })
+                throw err;
+            }
+            res.json({ data: result[0], result: 'success' });
+        });
+    });
+}
+
 function Render(req, res, other) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
@@ -70,6 +85,89 @@ function hideUpdate(req,res) {
         });
     });
 }
+
+function board_update_getClassAndpassword(req,res) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("people").collection("manager_information");
+        var findThing = { board_ID: req.body.board_ID };
+        table.find(findThing, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (result[0]['password_private'] == req.body.password) {
+                board_update_updateMain(req, res, req.body.type, result[0]['class'], result[0]['type']);
+            }
+        });
+    });
+}
+
+function board_update_updateMain(req, res, type, name, right) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db("people").collection("manager_information");
+        var filter = { board_ID: req.body.board_ID };
+        var goal = {  };
+        goal[req.body.type] = req.body.include;
+        table.updateOne(filter, { $set: goal }, function (err, result) {
+            if (err) {throw err;}
+            db.close();
+            switch (type) {
+                case 'title':
+                    board_update_updateTwo(req, res, "data", name + '_board', type, right);
+                    break;
+                case 'include':
+                case 'tab':
+                    if (right == 'public')
+                        board_update_updateOne(req, res, "data", name + '_board', type);
+                    break;
+                case 'introcuce':
+                    board_update_updateOne(req, res, "board", 'all_board_number', type);
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+}
+
+function board_update_updateTwo(req, res, database, collection, type,right) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db('board').collection('all_board_number');
+        var filter = { board_ID: req.body.board_ID };
+        var goal = {};
+        goal[type] = req.body.include;
+        table.updateOne(filter, { $set: goal }, function (err, result) {
+            if (err) {
+                res.json({ result: 'error' });
+                throw err;
+            }
+            db.close();
+            //console.log(result);
+            if (right == 'public')
+                board_update_updateOne(req, res, database, collection, type);
+            else
+                res.json({ result: 'success' });
+        });
+    });
+}
+
+function board_update_updateOne(req, res, database, collection, type) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var table = db.db(database).collection(collection);
+        var filter = { board_ID: req.body.board_ID };
+        var goal = {};
+        goal[type] = req.body.include;
+        table.updateOne(filter, { $set: goal }, function (err, result) {
+            if (err) {
+                res.json({ result: 'error' });
+                throw err;
+            }
+            db.close();
+            //console.log(result);
+            res.json({ result: 'success' });
+        });
+    });
+}
 //router
 //前往個人看板管理
 router.post('/to_personal_manage', function (req, res) {
@@ -91,11 +189,11 @@ router.post('/post_display', function (req, res) {
 });
 //更改看板相關資訊
 router.post('/board_update', function (req, res) {
-
+    board_update_getClassAndpassword(req, res);
 });
 //取得看板資訊
 router.post('/board_information', function (req, res) {
-
+    ReJSON(req, res);
 });
 
 module.exports = router;
