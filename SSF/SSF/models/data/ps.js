282771583ba471e8dd9092aa011711140acc0e28
juju70302 <59501334+juju70302@users.noreply.github.com>
2020-09-08 18:13:45 +0800

    Delete ps.js

--- SSF/SSF/models/data/ps.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const page_model = mongoose.model('ps', mongoose.Schema({
-    type: {
-        type: String,
-        required: true
-    }
-}))
-
-module.exports=page_model;
