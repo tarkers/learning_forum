17b5d4921b30b2f2b6c95affb46b36d7bba49f7c
leon123858 <a0970785699@gmail.com>
2020-09-10 21:22:44 +0800

    change db mode from local to altis

--- SSF/SSF/routes/DB.js
+++ SSF/SSF/routes/DB.js
@@ -4,8 +4,8 @@ var router = express.Router();
 var urlencode = require('urlencode');
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
-var uri = "mongodb://localhost:27017/";
-
+const {GetUrl} = require('./database_url');
+var uri = GetUrl('null');
 //function block
 //mode 0:  1:密碼錯誤或帳號不存在,按上一頁再試一次 2:伺服器連線問題請,請按上一頁再試一次 3:操作不合法,請聯絡網站管理者
 function warming(res, mode) {
